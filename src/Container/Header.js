ce325b1a695094e8218c852bb1ec41ebccd8ecda
kumar pudi <kuladeepkrishnakumar@gmail.com>
2023-10-04 10:31:58 +0530

    cs  changed

--- src/Container/Header.js
+++ src/Container/Header.js
@@ -5,14 +5,10 @@ const Header = ({ count }) => {
   return (
     <div>
       <nav className="navbar navbar-light">
-        <div style={ {width:"75%",alignItems:"center" }}>
-          <h3 style={{textAlign:"center" ,paddingLeft:"30%"}}>Restaurant</h3>
-        </div>
-        <div style={{right:'0', width:"auto"}} >
+          <h3 style={{paddingLeft:'7%'}}className='mx-auto'>Restaurant</h3>
           <button className="btn btn-secondary" >
             <Link to="/orders"style={{ color: "white", textDecoration:'none' }}>Orders</Link><span className="badge bg-secondary">{count}</span>
           </button>
-        </div>
       </nav>
     </div>
   );
